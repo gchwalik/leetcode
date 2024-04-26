b618984304d0542c1fa8eb3a45e3ff2a5c49a910
unfamiliarish <89762294+unfamiliarish@users.noreply.github.com>
2024-04-25 18:59:12 -0700

    simpler, but a lot slower, because keeps checking when doesn't need to?

--- binary_search/704.py
+++ binary_search/704.py
@@ -6,27 +6,17 @@ def search(arr: list[int], val: int) -> int:
     # if val found, return index
     # else, return -1
     def recurse(arr: list[int], val: int, s: int, e: int, count=0):
-        # if count == 10:
-        #     return float("inf")
+        if s > e:
+            return -1
 
-        # print(f"arr:{arr},val:{val},s:{s},e:{e}")
-        if arr[s] == val:
-            return s
-        if arr[e] == val:
-            return e
-        if (e-s) <= 1:
-            return -1 
-        
-        m = (e-s) // 2 + s
-        # print(f"m:{m}")
+        m = (s+e) // 2
         middle_val = arr[m]
         if middle_val == val:
             return m
         elif val > middle_val:
             return recurse(arr, val, m+1, e, count+1)
-        elif val < middle_val:
+        else:
             return recurse(arr, val, s, m-1, count+1)
-        return -1
 
     return recurse(arr, val, 0, len(arr)-1)
 
