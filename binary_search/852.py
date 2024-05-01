3f80b43f6db67d151a9c6fc90604ca94068adc37
unfamiliarish <89762294+unfamiliarish@users.noreply.github.com>
2024-05-01 16:51:22 -0700

    find mountain peak index

---
+++ binary_search/852.py
@@ -0,0 +1,53 @@
+# An array arr is a mountain if the following properties hold:
+# - arr.length >= 3
+# - there exists some i with 0 < i < arr.length - 1 such that:
+#   - arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
+#   - arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+# Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+
+# You must solve it in O(log(arr.length)) time complexity.
+
+
+def get_slope(slice: list[int]) -> int:
+    s, m, e = slice
+    if s > m:
+        return -1
+    if e > m:
+        return 1
+    return 0
+
+def get_peak_index(arr: list[int]) -> int:
+    # m - array of ints describing the shape of the mountain 
+    # start and end cannot be the peak 
+    def get_slice(m:int) -> list[int]:
+        # [0,1,2,3,4,5], m=2
+        # return [1,2,3]
+        return arr[m-1:m+2]
+
+    def recurse(s: int, e: int) -> int:
+        # returns index of mountain peak 
+        m = (e-s) // 2 + s
+        slice = get_slice(m)
+        slope = get_slope(slice)
+        if slope == 0:
+            return m
+        elif slope > 0:
+            # mountain rising, so slice to the right
+            return recurse(m, e)
+        else:
+            # mountain falling, so slice to the left
+            return recurse(s, m)
+
+    return recurse(0, len(arr)-1)
+
+
+assert get_peak_index([0,1,0]) == 1
+assert get_peak_index([0,1,2,0]) == 2
+assert get_peak_index([0,10,5,2]) == 1
+
+
+
+
+
+
