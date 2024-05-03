0da109d8ba573864a57fd500b581232293b045b5
unfamiliarish <89762294+unfamiliarish@users.noreply.github.com>
2024-05-02 17:48:20 -0700

    no change, updated commit message - not actually slow, there's too much variability with the runtime on leetcode, so actually fine :)

--- dfs/100.py
+++ dfs/100.py
@@ -1,5 +1,5 @@
 # Given the roots of two binary trees p and q, write a function to check 
-# if they are the same or not.
+# if they are the same or not. 
 
 # Two binary trees are considered the same if they are structurally identical, 
 # and the nodes have the same value.
